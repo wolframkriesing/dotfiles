b0a04a87cc8d5a77036468a5299327ca36544456
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-12-03 15:44:59 +0100

    add vim directory, which we do use

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,7 @@
 
 dir=~/programming/my-repos/dotfiles # dotfiles directory
 olddir=~/dotfiles_old               # old dotfiles backup directory
-files="vimrc tmux.conf"       # list of files/folders to symlink in homedir
+files="vim vimrc tmux.conf"             # list of files/folders to symlink in homedir
 
 ##########
 
